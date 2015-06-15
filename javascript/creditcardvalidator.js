7b954a31dfc6050c6037a9e968daf5bc411595dc
Jacob Meline <jacob.meline@gmail.com>
2015-06-15 10:43:04 -0600

    Got to rank 6kyu after completing creditcardvalidator.js

---
+++ javascript/creditcardvalidator.js
@@ -0,0 +1,38 @@
+exports.validate = function(n){
+   var convertInt2IntArray = function(n){
+        return ("" + n).split("").map(Number);
+    };
+
+    var arrayOfIntegers = convertInt2IntArray(n);
+    var length = arrayOfIntegers.length;
+
+    console.log("Results: " + arrayOfIntegers);
+    var addArrayElements = function (tmp){
+        console.log("tmp: " + tmp + " length: " + tmp.length);
+        var total = 0;
+        for(var i = 0; i < tmp.length; i++)
+            total += tmp[i];
+        return total;
+    };
+    var isEven = length % 2 === 0;
+    var modifiedArray = arrayOfIntegers.map(function(value, index){
+        var new_value = value;
+        if (isEven && index % 2 === 0)
+            new_value = value + value;
+        else if(!isEven && index % 2 !== 0)
+            new_value = value + value;
+        if(new_value > 9)
+        {
+            var intArray = convertInt2IntArray(new_value);
+            new_value = addArrayElements(intArray);
+        }
+
+        return new_value;
+    });
+
+    console.log(modifiedArray);
+    total = addArrayElements(modifiedArray);
+    console.log(total);
+    return total % 10 === 0;
+};
+
--- javascript/test.js
+++ javascript/test.js
@@ -1,6 +1,7 @@
 var assert = require("/usr/local/lib/node_modules/chai").assert;
 var cipher = require('./cipher.js');
 var phoneNumber = require('./createPhoneNumber.js');
+var creditcardvalidation = require('./creditcardvalidator.js');
 
 describe("Kata testing...", function(){
     describe("Simple Substitution Cipher", function(){
@@ -10,30 +11,48 @@ describe("Kata testing...", function(){
             var abc1 = "abcdefghijklmnopqrstuvwxyz";
             var abc2 = "etaoinshrdlucmfwypvbgkjqxz";
             sub = new cipher.SubstitutionCipher(abc1, abc2);
-        })
+        });
         // console.log(sub);
         describe("Test Substitution Cipher Encoding...", function(){
             it('Encoding examples', function(){
                 assert.equal(sub.encode("abc"), "eta");
                 assert.equal(sub.encode("_b_"), "_t_");
-            })  
-        })
+            });
+        });
         describe("Test Substitution Cipher Decoding...", function(){
             it('Decoding examples', function(){
                 assert.equal(sub.decode("kjpphi"), 'vwrrhe');
                 assert.equal(sub.decode("aeiou"), "caedl");
 
-            })
-        })
+            });
+        });
 
-    })
+    });
     describe("Simple Create a Phone Number", function(){
-        var phone
         it('Test creating regex', function(){
             var numbers = [1,2,3,4,5,6,7,8,9,0];
 
             assert.equal("aaa".replace(/a(a)a/, 'b($1)($1)b'), 'b(a)(a)b');
-            assert.equal(phoneNumber.createPhoneNumber(numbers), "(123) 456-7890");   
-        })
-    })
-})
+            assert.equal(phoneNumber.createPhoneNumber(numbers), "(123) 456-7890");
+        });
+    });
+
+    describe("Validating Credit Card Numbers", function(){
+        it('Test validating using 891', function(){
+            var number = 891;
+            assert.equal(creditcardvalidation.validate(number), false);
+        });
+        it('Test validating using 1', function(){
+            var number = 1;
+            assert.isFalse(creditcardvalidation.validate(number));
+        });
+        it('Test validating using 2,1,2,1', function(){
+            var number = 2121;
+            assert.isTrue(creditcardvalidation.validate(number));
+        });
+        it('Test validating using 1,2,3,0', function(){
+            var number = 1230;
+            assert.isTrue(creditcardvalidation.validate(number));
+        });
+    });
+});
