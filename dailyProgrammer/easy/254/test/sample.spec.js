4dfca294efa7b683f5dfa7ea9fed809bda8147cb
Jacob Meline <jacob.meline@gmail.com>
2016-03-02 08:05:59 -0700

    adding daily programmer files separated into folders

---
+++ dailyProgrammer/easy/254/test/sample.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+describe('AtbashCipher', () => {
+  describe('sample', () => {
+    it('adds to 2', () => {
+      expect('hi').to.be.a('string');
+    });
+    it('adds to 3', () => {
+      expect(1).to.be.a('number');
+    });
+  });
+
+});
