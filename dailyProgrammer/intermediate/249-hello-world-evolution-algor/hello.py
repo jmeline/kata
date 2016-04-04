714a9954dd1b5a787f5b203de95016ef1528801b
Jacob Meline <jacob.meline@gmail.com>
2016-04-03 18:12:59 -0600

    Refactored the evolutionary algorithm and learned some new python tricks

--- dailyProgrammer/intermediate/249-hello-world-evolution-algor/hello.py
+++ dailyProgrammer/intermediate/249-hello-world-evolution-algor/hello.py
@@ -7,35 +7,41 @@ def getRandomChar():
     strings = string.printable
     return ''.join(random.choice(strings))
 
+def getRandomString():
+    return ''.join([getRandomChar() for _ in range(len(result))])
+
 def hammingDistance(s1, s2):
     if len(s1) != len(s2):
         raise ValueError("Unknown Value")
-    return [ bool(ord(ch1) - ord(ch2)) for ch1, ch2 in zip(s1, s2) ]
+    return [ abs(ord(ch1) - ord(ch2)) for ch1, ch2 in zip(s1, s2) ]
 
 def mutatePopulation(population, fitnessArray):
     newPopulation = ''
-    for fitness, pop in zip(fitnessArray, population):
-        if (not fitness):
+    zippedValues = zip(population, fitnessArray)
+    for pop, fitness in zippedValues:
+        if (fitness == 0):
             newPopulation += pop
         else:
             newPopulation += getRandomChar()
     return newPopulation
 
 def runGenerations():
-    bestFitness = len(result) * 1000
+    fitnessTest = lambda population: hammingDistance(population, result)
+    fitness = bestFitness = len(result) * 1000
     currentGeneration = 1
 
     # initial population
-    population = ''.join([getRandomChar() for _ in range(len(result))])
-    fitness = bestFitness
+    population = getRandomString()
     while(fitness > 0):
         # fitness test the population
-        fitnessArray = hammingDistance(population, result)
+        fitnessArray = fitnessTest(population)
         fitness = reduce(lambda x,y: x+y, fitnessArray)
+        # evaluate the fitness test
         if (fitness < bestFitness):
             bestFitness = fitness
             print ("Gen: {0} | Fitness: {1} | {2}" \
                     .format(currentGeneration, fitness, population))
+        # Mutate and kill off unfit members of the population
         population = mutatePopulation(population, fitnessArray)
         currentGeneration += 1
 runGenerations()
